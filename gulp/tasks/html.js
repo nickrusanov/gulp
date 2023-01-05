4f831983571d508682c9102d10c36a64977499b5
Nikolay <nickrusanov94@gmail.com>
2023-01-05 12:25:35 +0400

    del webp-html package

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,10 +1,8 @@
 import fileInclude from 'gulp-file-include';
 import htmlmin from 'gulp-htmlmin';
-import webpHtml from 'gulp-webp-html';
 
 export default () =>
 	app.gulp.src(app.path.html.src)
 		.pipe(fileInclude())
-		.pipe(webpHtml())
 		.pipe(htmlmin(app.plugins.htmlmin))
 		.pipe(app.gulp.dest(app.path.html.dist))
